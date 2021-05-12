ad8820ff12bfd6b08e90ef74f06d50d32b6c519d
luke <lukeperrin27@gmail.com>
2021-05-12 22:06:03 +0200

    adds success message popup on article creation

--- src/components/PopupMessage.jsx
+++ src/components/PopupMessage.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
 import { Portal, Segment, Button, Header } from 'semantic-ui-react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
-const PopupMessage = () => {
-	const [popup, setPopup] = useState(false);
+const PopupMessage = (props) => {
+	const [popup, setPopup] = useState(true);
 	const message = useSelector((state) => state.message);
+	const popupOpen = useSelector((state) => state.popupOpen)
+	const dispatch = useDispatch()
+
 
 	return (
-		<Portal closeOnTriggerClick openOnTriggerChange trigger={<Button>Hello</Button>}>
+		<Portal closeOnDocumentClick 
+		 onClose={() => dispatch({type: 'CLOSE_MESSAGE'})} 
+		open={popupOpen}>
 			<Segment
 				style={{
 					left: '40%',
--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,17 +1,21 @@
-
 const rootReducer = (state, action) => {
-  switch (action.type) {
-    case 'SUCCESS_MESSAGE':
-      return {
-        ...state, 
-        message: action.payload
-      }
-      
-  
-    default:
-      break;
-  }
-  return state;
+	switch (action.type) {
+		case 'SUCCESS_MESSAGE':
+			return {
+				...state,
+				message: action.payload,
+				popupOpen: true,
+			};
+
+    case 'CLOSE_MESSAGE': 
+    return {
+      ...state,
+      popupOpen: false
+    }
+
+		default:
+			return state;
+	}
 };
 
 export default rootReducer;
