7cc1782568de50e6c29fdd4b03b2c529806d2057
sevader14 <vsevolod.deriushkin@gmail.com>
2021-05-12 19:23:29 +0200

    adds fixture for /articles POST request adds axios

--- cypress/integration/userCanCreateArticle.feature.js
+++ cypress/integration/userCanCreateArticle.feature.js
@@ -1,20 +1,24 @@
 describe('User can create article', () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit('/');
+
+    cy.intercept('POST', 'https://fake-newzzzz.herokuapp.com/api/articles', {
+      fixture: { message: 'Your article has successfully been created' },
+    });
+  });
 
   describe('successfully', () => {
     it('can fill the article creation form and submit', () => {
-      cy.get('[data-cy=create-article-btn]').click()
-      cy.get('[data-cy=article-creation-modal]').should('be.visible')
+      cy.get('[data-cy=create-article-btn]').click();
+      cy.get('[data-cy=article-creation-modal]').should('be.visible');
       cy.get('[data-cy=article-creation-form]').within(() => {
-        cy.get('[data-cy=title]').type('Title')
-        cy.get('[data-cy=teaser]').type('CIA is spying on you')
-        cy.get('[data-cy=body]').type('No for real! Get a tinfoil hat quick!')
-        cy.get('[data-cy=categories]').select('Flat Earth')
-        cy.get(['data-cy=submit-btn']).click()
-      })
-      cy.get('[data-cy=article-creation-modal]').should('not.be.visible')
+        cy.get('[data-cy=title]').type('Title');
+        cy.get('[data-cy=teaser]').type('CIA is spying on you');
+        cy.get('[data-cy=body]').type('No for real! Get a tinfoil hat quick!');
+        cy.get('[data-cy=categories]').select('Flat Earth');
+        cy.get(['data-cy=submit-btn']).click();
+      });
+      cy.get('[data-cy=article-creation-modal]').should('not.be.visible');
     });
-  })  
-})
+  });
+});
--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://fake-newzzzz.herokuapp.com/api'
 
 ReactDOM.render(
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
