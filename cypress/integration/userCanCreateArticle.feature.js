45030dffe6fb767a4612bef11d44ddef2317a291
sevader14 <vsevolod.deriushkin@gmail.com>
2021-05-13 20:42:45 +0200

    expands before block in userCanCreateArticle to perform login adds sad path tests for login function adds PopupMessage component to App, to be globally visible

--- cypress/integration/userCanCreateArticle.feature.js
+++ cypress/integration/userCanCreateArticle.feature.js
@@ -1,6 +1,18 @@
 describe('User can create article', () => {
   beforeEach(() => {
     cy.visit('/');
+    cy.intercept(
+      'POST',
+      'https://fake-newzzzz.herokuapp.com/api/auth/sign_in',
+      {
+        fixture: 'handleLogin.json',
+      }
+    );
+    cy.get('[data-cy=login-form]').within(() => {
+      cy.get('[data-cy=login-username]').type('user@mail.com');
+      cy.get('[data-cy=login-password]').type('password');
+      cy.get('[data-cy=login-btn]').click();
+    });
   });
 
   describe('successfully', () => {
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import LogInLandingpage from './components/LogInLandingpage'
 import Dashboard from './components/Dashboard';
+import PopupMessage from './components/PopupMessage'
 import { Switch, Route } from 'react-router';
 
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
       <Route exact path="/" component={LogInLandingpage}></Route>
       <Route exact path="/dashboard" component={Dashboard}></Route>
     </Switch>
-      
+    <PopupMessage/>
     </>
   );
 };
