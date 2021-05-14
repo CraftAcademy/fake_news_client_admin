4866ea214b3f7931325da6b00bfb4ad10bbde54f
sevader14 <vsevolod.deriushkin@gmail.com>
2021-05-14 20:40:20 +0200

    adds tests and fixture listOfArticles.json

---
+++ cypress/integration/userCanSeeTheirArticles.feature.js
@@ -0,0 +1,33 @@
+describe('User can see their articles', () => {
+  beforeEach(() => {
+    cy.visit('/dashboard');
+  });
+
+  describe('Successfully', () => {
+    beforeEach(() => {
+      cy.intercept('GET', 'https://fake-newzzzz.herokuapp.com/api/articles', {
+        fixture: 'listOfArticles.json',
+      });
+    });
+
+    it('is expected to show the list of articles', () => {
+      cy.get('[data-cy=article]').should('have.length', 4);
+    });
+
+    it('is expected to show the first article', () => {
+      cy.get('[data-cy=article]')
+        .first()
+        .within(() => {
+          cy.get('[data-cy=title]').should(
+            'contain',
+            "Amateur Rocket-Maker Finally Launches Himself Off Earth - Now To Prove It's Flat"
+          );
+          cy.get('[data-cy=date]').should('2021-05-13, 20:03');
+        });
+    });
+
+    it('is expected to show the name of the journaist', () => {
+      cy.get('[data-cy=greeting]').should('contain', 'Mr. Fake')
+    })
+  });
+});
