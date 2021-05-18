410af180dea944a4400dd37a64b5d2e3467183c4
lucas <laksteelhouse@gmail.com>
2021-05-18 10:57:00 +0200

    adds some box shadows to dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -42,7 +42,7 @@ const JournalistDashboard = () => {
       {!authenticated && <Redirect to='/' />}
       <Grid centered>
         <Grid.Row centered>
-          <h1 style={{ color: 'white', fontSize: 40, marginTop: 25 }}>
+          <h1 style={{ color: 'white', fontSize: 50, marginTop: 25 }}>
             FAKE
             <span style={{ color: '#FCE42D' }}> ? </span>
             NEWS
@@ -68,8 +68,12 @@ const JournalistDashboard = () => {
         as={Segment}
         style={{
           maxHeight: 550,
+          marginTop: 25,
           overflowY: 'scroll',
           backgroundColor: '#202325',
+          padding: 15,
+          boxShadow:
+            '-1px -1px 2px rgba(255, 255, 255, 0.25), inset -1px -1px 5px rgba(255, 255, 255, 0.25), 8px 30px 30px rgba(0, 0, 0, 0.4), inset -2px -2px 5px rgba(0, 0, 0, 0.3)',
         }}>
         {articles[0] ? (
           <Item.Group>{listOfArticles}</Item.Group>
